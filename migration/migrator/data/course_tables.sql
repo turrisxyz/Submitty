c04b61430843ab88fd40f00d98c2b43719dec06e
sbelsk <shellybelsky02@gmail.com>
2022-02-08 09:17:32 -0500

    dump migration

--- migration/migrator/data/course_tables.sql
+++ migration/migrator/data/course_tables.sql
@@ -51,60 +51,60 @@ $_$;
 CREATE FUNCTION public.csv_to_numeric_gradeable(vcode text[], gradeable_id text, grader_id text) RETURNS boolean
     LANGUAGE plpgsql
     AS $$
-  DECLARE
-    -- Size of first array after splitting
-    size INTEGER;
-    -- Array of individual line after splitting
-    line TEXT[];
-    -- Variable to store each line in the array
-    i TEXT;
-    -- Array of gc_ids for this gradeable
-    gcids INTEGER[];
-    -- gradeable_data id for this gradeable for this student
-    gdid INTEGER;
-    -- Array counter
-    j INTEGER;
-    -- Is this gradeable component text?
-    istext BOOLEAN[];
-    --Score to be inserted
-    score NUMERIC;
-  BEGIN
-    gcids := ARRAY(SELECT gc_id FROM gradeable_component WHERE g_id = gradeable_id);
-    istext := ARRAY(SELECT gc_is_text FROM gradeable_component WHERE g_id = gradeable_id);
-    -- Get the number of gradeable components for this gradeable. Will be used to test
-    -- for uniform sized arrays
-    size := array_length(gcids, 1);
-    FOREACH i IN ARRAY vcode
-      LOOP
-        -- Split the current line
-        line := string_to_array(i, ',');
-        -- Check for uniform size
-        IF array_length(line, 1) <> size + 1 THEN
-          RAISE EXCEPTION 'INVALID SIZE: Arrays are jagged.';
-        END IF;
-
-        -- Remove any existing record for this student for this gradeable
-        DELETE FROM gradeable_data WHERE gd_user_id = line[1] AND g_id = gradeable_id;
-
-        INSERT INTO gradeable_data(g_id, gd_user_id) VALUES (gradeable_id, line[1]);
-
-        SELECT gd_id INTO gdid FROM gradeable_data WHERE g_id = gradeable_id AND gd_user_id = line[1];
-
-        FOR j IN 1..size
+    DECLARE
+        -- Size of first array after splitting
+        size INTEGER;
+        -- Array of individual line after splitting
+        line TEXT[];
+        -- Variable to store each line in the array
+        i TEXT;
+        -- Array of gc_ids for this gradeable
+        gcids INTEGER[];
+        -- gradeable_data id for this gradeable for this student
+        gdid INTEGER;
+        -- Array counter
+        j INTEGER;
+        -- Is this gradeable component text?
+        istext BOOLEAN[];
+        --Score to be inserted
+        score NUMERIC;
+    BEGIN
+        gcids := ARRAY(SELECT gc_id FROM gradeable_component WHERE g_id = gradeable_id);
+        istext := ARRAY(SELECT gc_is_text FROM gradeable_component WHERE g_id = gradeable_id);
+        -- Get the number of gradeable components for this gradeable. Will be used to test
+        -- for uniform sized arrays
+        size := array_length(gcids, 1);
+        FOREACH i IN ARRAY vcode
         LOOP
-          IF istext[j] THEN
-          --COME BACK AND FIX: need to put in gcd_grade_time...double check to see that CSV upload still works for numeric/text
-            INSERT INTO gradeable_component_data(gc_id, gd_id, gcd_component_comment, gcd_grader_id, gcd_graded_version, gcd_grade_time) VALUES (gcids[j], gdid, line[j+1], grader_id, NULL);
-          ELSE
-            score := CAST(line[j+1] AS NUMERIC);
-            INSERT INTO gradeable_component_data(gc_id, gd_id, gcd_score, gcd_grader_id, gcd_graded_version, gcd_grade_time) VALUES (gcids[j], gdid, score, grader_id, NULL);
-          END IF;
-        END LOOP;
+            -- Split the current line
+            line := string_to_array(i, ',');
+            -- Check for uniform size
+            IF array_length(line, 1) <> size + 1 THEN
+            RAISE EXCEPTION 'INVALID SIZE: Arrays are jagged.';
+            END IF;
 
-      END LOOP;
-    RETURN TRUE ;
-  END;
-  $$;
+            -- Remove any existing record for this student for this gradeable
+            DELETE FROM gradeable_data WHERE gd_user_id = line[1] AND g_id = gradeable_id;
+
+            INSERT INTO gradeable_data(g_id, gd_user_id) VALUES (gradeable_id, line[1]);
+
+            SELECT gd_id INTO gdid FROM gradeable_data WHERE g_id = gradeable_id AND gd_user_id = line[1];
+
+            FOR j IN 1..size
+            LOOP
+            IF istext[j] THEN
+            --COME BACK AND FIX: need to put in gcd_grade_time...double check to see that CSV upload still works for numeric/text
+                INSERT INTO gradeable_component_data(gc_id, gd_id, gcd_component_comment, gcd_grader_id, gcd_graded_version, gcd_grade_time) VALUES (gcids[j], gdid, line[j+1], grader_id, NULL);
+            ELSE
+                score := CAST(line[j+1] AS NUMERIC);
+                INSERT INTO gradeable_component_data(gc_id, gd_id, gcd_score, gcd_grader_id, gcd_graded_version, gcd_grade_time) VALUES (gcids[j], gdid, score, grader_id, NULL);
+            END IF;
+            END LOOP;
+
+        END LOOP;
+        RETURN TRUE ;
+    END;
+    $$;
 
 
 --
@@ -150,7 +150,7 @@ CREATE TABLE public.categories_list (
     category_id integer NOT NULL,
     category_desc character varying NOT NULL,
     rank integer,
-    color character varying DEFAULT '#000080'::character varying NOT NULL
+    color character varying NOT NULL
 );
 
 
